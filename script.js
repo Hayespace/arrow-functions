01e438b93e4a74647eecb55878bea5ce3a70a1e3
Hayespace <ben.hayes@hotmail.com>
2023-06-26 17:04:13 +0000

    add sounds and styling

--- script.js
+++ script.js
@@ -13,3 +13,43 @@ function removeActiveClasses() {
 
     })
 }
+
+function playSound(e){
+   
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+  
+   
+    if(!audio) return;
+    audio.currentTime = 0
+    audio.play()      
+
+}
+  
+window.addEventListener('keydown', playSound)
+
+ var kick = new Audio();
+ kick.src = 'assets/sounds/library-1/Kick.wav';
+
+ var snare = new Audio();
+ snare.src = 'assets/sounds/library-1/snare.wav';
+
+ var chh = new Audio();
+ chh.src = 'assets/sounds/library-1/ClosedHH.wav';
+
+ var hh = new Audio();
+ hh.src = 'assets/sounds/library-1/HH.wav';
+
+ var crash = new Audio();
+ crash.src = 'assets/sounds/library-1/Crash.wav';
+
+ var clap = new Audio();
+ clap.src = 'assets/sounds/library-1/clap.wav';
+
+ var conga = new Audio();
+ conga.src = 'assets/sounds/library-1/Conga.wav';
+
+ var cowbell = new Audio();
+ cowbell.src = 'assets/sounds/library-1/Cowbell.wav';
+
+ var shaker = new Audio();
+ shaker.src = 'assets/sounds/library-1/Shaker.wav';
